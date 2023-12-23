24ddaa651922a19b46f7f2f0ae004ce97819b928
Alamin-Ahammed <ahammedalamin2002@gmail.com>
2023-12-23 10:07:12 +0600

    added pagination in not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import notFound from '../../../public/404.gif'
+import React from "react";
+import notFound from "../../../public/404.gif";
+import Pagination from "../../components/Pagination/Pagination";
 
 export default function NotFound() {
   return (
-    <div style={{width: '100%',height: '100vh',background: '#010002',display: 'flex',justifyContent: 'center',alignItems: 'center'}} >
-      <img  src={notFound} alt="Not Found" />
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        background: "#010002",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <img src={notFound} alt="Not Found" />
+      <Pagination />
     </div>
-  )
+  );
 }
